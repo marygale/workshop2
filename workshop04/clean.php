c58c945a1bc8c0090769b326d8daad6836217f8e
gale <mary.jabagat@ripeconcepts.com>
2018-01-09 19:04:20 +0800

    add clean.php

--- workshop04/clean.php
+++ workshop04/clean.php
@@ -7,14 +7,14 @@ try {
     //$con = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
     // set the PDO error mode to exception
     //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "DELETE * FROM autos";
+    //$sql = "DELETE * FROM autos";
     // use exec() because no results are returned
     //$conn->exec($sql);
     //echo "Record deleted successfully";
 
     $con = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
     $con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    $sql = "DELETE * FROM autos";
+    $sql = "DELETE FROM autos;";
     $query = $con->prepare( $sql );
     $query->execute();
     echo "Record deleted successfully";
